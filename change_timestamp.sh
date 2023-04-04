f3ac268bb85430b72775c3ddf3ce200e9685349b
Zakaria Kebairia <4.kebairia@gmail.com>
2023-04-05 00:16:11 +0100

    fix the output folder in `change_timestamps.sh` script

--- change_timestamp.sh
+++ change_timestamp.sh
@@ -4,7 +4,7 @@
 # Email: 4.kebairia@gmail.com
 #
 # Description: 
-#
+# later
 for file in $(\ls ./content/*.org | awk -F "/" '{ print $NF }' | grep -v index.org) ; do
     _date=$( echo "$file" | cut -d"-" -f1-3 )
      touch -t $(date -d $_date +"%Y%m%d%H%M") ./content/$file
