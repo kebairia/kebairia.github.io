5ef7959ae94c17451f83201ec6b4352937ba5c7c
Zakaria Kebairia <4.kebairia@gmail.com>
2023-04-04 14:38:08 +0100

    blah

--- build-site.el
+++ build-site.el
@@ -1,3 +1,4 @@
+
 ;; Set the package installation directory so that packages aren't stored in the
 ;; ~/.emacs.d/elpa path.
 (require 'package)
@@ -16,13 +17,27 @@
 ;; Load the publishing system
 (require 'ox-publish)
 
+;; Customize the HTML output
+(setq org-html-validation-link nil            ;; Don't show validation link
+      org-html-head-include-scripts nil       ;; Use our own scripts
+      org-html-head-include-default-style nil ;; Use our own styles
+      org-html-head "<link rel=\"stylesheet\" href=\"https://cdn.simplecss.org/simple.min.css\" />")
+
+;; Define the publishing project
 (setq org-publish-project-alist
       (list
-       (list "my-org-site"
+       (list "org-site:main"
              :recursive t
              :base-directory "./content"
+             :publishing-function 'org-html-publish-to-html
              :publishing-directory "./public"
-             :publishing-function 'org-html-publish-to-html)))
+             :with-author nil           ;; Don't include author name
+             :with-creator t            ;; Include Emacs and Org versions in footer
+             :with-toc t                ;; Include a table of contents
+             :section-numbers nil       ;; Don't include section numbers
+             :time-stamp-file nil)))    ;; Don't include time stamp in file
 
+;; Generate the site output
 (org-publish-all t)
-(message "Build completed")
+
+(message "Build complete!")
