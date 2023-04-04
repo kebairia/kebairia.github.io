7087dc1715dd015ce58ccd67a6a288e05f6e9059
Zakaria Kebairia <4.kebairia@gmail.com>
2023-04-04 15:09:40 +0100

    use strainght for packages

--- build-site.el
+++ build-site.el
@@ -1,18 +1,31 @@
+(setq project-switch-commands t) 
 
-;; Set the package installation directory so that packages aren't stored in the
-;; ~/.emacs.d/elpa path.
-(require 'package)
-(setq package-user-dir (expand-file-name "./.packages"))
-(setq package-archives '(("melpa" . "https://melpa.org/packages/")
-                         ("elpa" . "https://elpa.gnu.org/packages/")))
+(setq package-enable-at-startup nil)
+;;-------------------------
+;; straight.el bootstraping
+;;-------------------------
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 6))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
 
-;; Initialize the package system
-(package-initialize)
-(unless package-archive-contents
-  (package-refresh-contents))
+;; use use-package along with straight
+(straight-use-package 'use-package)
+;; make `use-package` to automatically install all of your packages 
+;; without the need for adding `:straight t`.
+(setq straight-use-package-by-default t)
+
 
 ;; Install dependencies
-(package-install 'htmlize)
+(use-package htmlize)
 
 ;; Load the publishing system
 (require 'ox-publish)
