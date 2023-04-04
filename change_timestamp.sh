9f7af633792841cd4a5e80269370017657dc2d29
Zakaria Kebairia <4.kebairia@gmail.com>
2023-04-05 00:07:29 +0100

    fix the timestamp issue

---
+++ change_timestamp.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Author: Zakaria Kebairia
+# Email: 4.kebairia@gmail.com
+#
+# Description: 
+#
+for file in $(\ls ./content/*.org | awk -F "/" '{ print $NF }' | grep -v index.org) ; do
+    _date=$( echo "$file" | cut -d"-" -f1-3 )
+     touch -t $(date -d $_date +"%Y%m%d%H%M") ./content/$file
+done
+
