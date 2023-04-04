8ceae55d340e32c55c7e694e585f2bda1009a75d
Zakaria Kebairia <4.kebairia@gmail.com>
2023-04-05 00:21:54 +0100

    Fixing and cleaning the `change_timestamp.sh` script
    Adding comment, make it better, clean and clear

--- change_timestamp.sh
+++ change_timestamp.sh
@@ -4,9 +4,24 @@
 # Email: 4.kebairia@gmail.com
 #
 # Description: 
-# later
-for file in $(\ls ./content/*.org | awk -F "/" '{ print $NF }' | grep -v index.org) ; do
-    _date=$( echo "$file" | cut -d"-" -f1-3 )
-     touch -t $(date -d $_date +"%Y%m%d%H%M") ./content/$file
+# This script updates the file modification time of all files in the ./content/ directory
+# based on their file name prefix (YYYY-MM-DD).
+
+# Define the content directory path
+content_dir="./content/"
+
+# Loop through each file in the content directory except for index.org
+for file in "${content_dir}"*.org; do
+    if [ "$file" != "${content_dir}index.org" ]; then
+
+        # Extract the date from the file name prefix
+        file_date="$(basename "$file" | cut -d'-' -f1-3)"
+
+        # Convert the date to the format required by the touch command
+        touch_date="$(date -d "${file_date}" +"%Y%m%d%H%M")"
+
+        # Update the file modification time using the touch command
+        touch -t "${touch_date}" "${file}"
+    fi
 done
 
