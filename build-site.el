5d168219e8301ea1dfc0d5b7fcb1b5444eae4579
Zakaria Kebairia <4.kebairia@gmail.com>
2023-05-13 17:58:37 +0100

    fixing colors

--- build-site.el
+++ build-site.el
@@ -1,4 +1,4 @@
-(setq project-switch-commands t) 
+(setq project-switch-commands t)
 
 (setq package-enable-at-startup nil)
 ;;-------------------------
@@ -53,33 +53,33 @@
       (list
 
        (list "blog-index"
-	     :recursive t
-	     :base-directory "./content/"
-	     :publishing-function 'org-html-publish-to-html
-	     :publishing-directory "./public"
-	     :auto-sitemap  t 
-	     :sitemap-filename "index.org"  
-	     :sitemap-title "Posts"         
-         :sitemap-sort-files   'anti-chronologically
-         :sitemap-format-entry 'zk/org-publish-org-sitemap-format-entry
-	     :auto-preamble nil
-	     :auto-postamble nil
-	     )
+	           :recursive t
+	           :base-directory "./content/"
+	           :publishing-function 'org-html-publish-to-html
+	           :publishing-directory "./public"
+	           :auto-sitemap  t 
+	           :sitemap-filename "index.org"  
+	           :sitemap-title "Posts"         
+             :sitemap-sort-files   'anti-chronologically
+             :sitemap-format-entry 'zk/org-publish-org-sitemap-format-entry
+	           :auto-preamble nil
+	           :auto-postamble nil
+	           )
        (list "blog-main"
-	     :author "Zakaria Kebairia"
-	     :email "4.kebairia@gmail.com"
-	     :recursive t
-	     :base-directory "./content"
-	     :publishing-function 'org-html-publish-to-html
-	     :publishing-directory "./public/"
-	     :with-author t
-	     :with-creator t
-	     :with-toc nil
-	     :with-date t
-	     :with-tags t
-	     :with-latex t
-	     :html-head-extra 
-	     "<header> <h4> <a href=\"https://kebairia.github.io\" title=\"Home\" class=\"home\">
+	           :author "Zakaria Kebairia"
+	           :email "4.kebairia@gmail.com"
+	           :recursive t
+	           :base-directory "./content"
+	           :publishing-function 'org-html-publish-to-html
+	           :publishing-directory "./public/"
+	           :with-author t
+	           :with-creator t
+	           :with-toc nil
+	           :with-date t
+	           :with-tags t
+	           :with-latex t
+	           :html-head-extra 
+	           "<header> <h4> <a href=\"https://kebairia.github.io\" title=\"Home\" class=\"home\">
 	          <img src=\"img/home.svg\" width=\"50\" alt=\"Home\" />
 	          </a>
 
@@ -88,22 +88,22 @@
 
 	          </a>
             <a href=\"files/cv.pdf\" title=\"My Resume\" type=\"application/pdf\" class=\"resume\">
-            <img src=\"img/cv.svg\" width=\"25\"  alt=\"My CV icon\" /> </a>
+            <img src=\"img/cv.svg\" width=\"40\"  alt=\"My CV icon\" /> </a>
 
             </h4>
 	          </header>"
-	     :section-numbers t
-	     :time-stamp-file t
-	     :auto-preamble nil
-	     :auto-postamble nil
-	     )
+	           :section-numbers t
+	           :time-stamp-file t
+	           :auto-preamble nil
+	           :auto-postamble nil
+	           )
        (list "blog-static"
-	     :recursive t
-	     :base-directory "./content/"
-	     :publishing-function 'org-publish-attachment
-	     :base-extension "css\\|js\\|png\\|jpg\\|gif\\|pdf\\|mp3\\|ogg\\|swf\\|woff\\|otf\\|svg\\|rss\\|xml"
-	     :publishing-directory "./public/"
-	     )
+	           :recursive t
+	           :base-directory "./content/"
+	           :publishing-function 'org-publish-attachment
+	           :base-extension "css\\|js\\|png\\|jpg\\|gif\\|pdf\\|mp3\\|ogg\\|swf\\|woff\\|otf\\|svg\\|rss\\|xml"
+	           :publishing-directory "./public/"
+	           )
 
        ;; (list "org"
        ;;       :components ("blog-index""blog-main" "blog-static"))
